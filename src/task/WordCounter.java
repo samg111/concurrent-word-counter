8ee096c13c1bf8b214d7bf1f4265ede9b8a8f6a6
samg111 <berrystump@protonmail.com>
2025-06-27 16:02:42 -0400

    added counting the unique words amongst all the files and adding their word count to hashmap

--- src/task/WordCounter.java
+++ src/task/WordCounter.java
@@ -1,5 +1,7 @@
 package task;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WordCounter implements Runnable {
@@ -14,5 +16,23 @@ public class WordCounter implements Runnable {
     @Override
     public void run() {
         System.out.println("Counting words in file: " + file.getName());
+        try {
+            countWordsInFile(file, wordCount);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + file.getName() + " - " + e.getMessage());
+        }
+    }
+
+    public static void countWordsInFile(File file, ConcurrentHashMap<String, Integer> wordCount) throws FileNotFoundException {
+        System.out.println("Counting words in file: " + file.getName());
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                String word = scanner.next().toLowerCase().replaceAll("[^a-z]", "");
+                if (!word.isEmpty()) {
+                    wordCount.merge(word, 1, Integer::sum);
+                }
+            }
+            System.out.println("wordCount: " + wordCount.size());
+        }
     }
 }
