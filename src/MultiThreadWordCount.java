a5b1202d3e688e0eeb0c9e5d5cbec33722c92540
samg111 <berrystump@protonmail.com>
2025-06-27 15:21:26 -0400

    added running an individual thread for each of the files

--- src/MultiThreadWordCount.java
+++ src/MultiThreadWordCount.java
@@ -5,16 +5,42 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MultiThreadWordCount {
     static ConcurrentHashMap<String, Integer> wordCount;
     public static void main(String[] args) {
-        // int numOfThreads = 5;
         wordCount = new ConcurrentHashMap<>();
 
-        // String inputFilesDirectory = "input_files/";
         System.out.println("pwd: " + System.getProperty("user.dir"));
 
         File inputDirectory = new File("input_files");
         File[] files = inputDirectory.listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
-        for (Object file : files) {
-            System.out.println("file: " + file.toString());
+
+        Thread[] threads = new Thread[files.length];
+        System.out.println("files length: " + files.length + " threads length: " + threads.length);
+
+        for (int i = 0; i < files.length; i++) {
+            threads[i] = new Thread(new WordCounter(files[i], wordCount));
+            threads[i].start();
+        }
+        for  (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.err.println("Thread interrupted: " + e.getMessage());
+            }
+        }
+
+    }
+
+    private static class WordCounter implements Runnable {
+        private final File file;
+        private final ConcurrentHashMap<String, Integer> wordCount;
+
+        public WordCounter(File file, ConcurrentHashMap<String, Integer> wordCount) {
+            this.file = file;
+            this.wordCount = wordCount;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Counting words in file: " + file.getName());
         }
     }
 }
