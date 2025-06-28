6458aef4cfc85109a7a240de3380f8b7d46cd329
samg111 <berrystump@protonmail.com>
2025-06-28 16:40:22 -0400

    added button for opening file selector

--- src/main/java/com/concurrentwordcounter/gui/JavaFxApp.java
+++ src/main/java/com/concurrentwordcounter/gui/JavaFxApp.java
@@ -1,5 +1,8 @@
 package com.concurrentwordcounter.gui;
 
+import java.io.File;
+import java.util.List;
+
 import com.concurrentwordcounter.threading.ThreadDelegator;
 
 import javafx.application.Application;
@@ -7,23 +10,37 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class JavaFxApp extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         Label label = new Label("welcome to the Concurrent Word Counter");
-        Button button = new Button("Run the Word Counter");
-        button.setOnAction(event -> {
+        Button runButton = new Button("Run the Word Counter");
+        runButton.setOnAction(event -> {
             ThreadDelegator.delegateTasks();
+        });
+        Button fileButton = new Button("Open File Selection");
+        fileButton.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Select Input Files");
+            List<File> selectedFiles = fileChooser.showOpenMultipleDialog(primaryStage);
+            if (selectedFiles != null && !selectedFiles.isEmpty()) {
+                for (File file : selectedFiles) {
+                    System.out.println("Selected file: " + file.getAbsolutePath());
+                }
+            } else {
+                System.out.println("No files selected.");
+            }
         });
 
 
         // StackPane root = new StackPane(label, button);
 
         VBox root = new VBox(10);
-        root.getChildren().addAll(label, button);
-        Scene scene = new Scene(root, 400, 200);
+        root.getChildren().addAll(label, runButton, fileButton);
+        Scene scene = new Scene(root, 800, 400);
 
         primaryStage.setTitle("Concurrent Word Counter");
         primaryStage.setScene(scene);
