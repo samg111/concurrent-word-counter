268de7618b5300671e110cb54e835fab5dc6078b
samg111 <berrystump@protonmail.com>
2025-06-29 14:05:27 -0400

    abstracted button creation to its own pkg

--- src/main/java/com/concurrentwordcounter/gui/JavaFxApp.java
+++ src/main/java/com/concurrentwordcounter/gui/JavaFxApp.java
@@ -1,51 +1,43 @@
 package com.concurrentwordcounter.gui;
 
-import java.io.File;
-
-import static com.concurrentwordcounter.MultiThreadWordCount.selectedFiles;
+import static com.concurrentwordcounter.ConcurrentWordCounter.selectedFiles;
+import com.concurrentwordcounter.gui.components.ButtonCreator;
 import com.concurrentwordcounter.threading.ThreadDelegator;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class JavaFxApp extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label label = new Label("welcome to the Concurrent Word Counter");
-        Button runButton = new Button("Run the Word Counter");
-        runButton.setOnAction(event -> {
-            ThreadDelegator.delegateTasks();
-        });
-        Button fileButton = new Button("Open File Selection");
-        fileButton.setOnAction(event -> {
+        Label label = new Label("Welcome to the Concurrent Word Counter");
+        Button runButton = ButtonCreator.createButton("Run the Word Counter", (event) -> {ThreadDelegator.delegateTasks();});
+        Button fileButton = ButtonCreator.createButton("Select input files", (event) -> {
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle("Select Input Files");
             selectedFiles = fileChooser.showOpenMultipleDialog(primaryStage);
-            if (selectedFiles != null && !selectedFiles.isEmpty()) {
-                for (File file : selectedFiles) {
-                    System.out.println("Selected file: " + file.getAbsolutePath());
-                }
-            } else {
-                System.out.println("No files selected.");
-            }
         });
-        Button quitButton = new Button("Quit");
-        quitButton.setOnAction(event -> {
-            Platform.exit();
-        });
+        Button quitButton = ButtonCreator.createButton("Quit", (event) -> {Platform.exit();});
+        label.setFont(new Font("Arial", 32));
+        fileButton.setFont(new Font("Arial", 22));
+        runButton.setFont(new Font("Arial", 22));
+        quitButton.setFont(new Font("Arial", 22));
 
 
         // StackPane root = new StackPane(label, button);
 
         VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
         root.getChildren().addAll(label, fileButton, runButton, quitButton);
-        Scene scene = new Scene(root, 800, 400);
+        Scene scene = new Scene(root, 800, 600);
 
         primaryStage.setTitle("Concurrent Word Counter");
         primaryStage.setScene(scene);
