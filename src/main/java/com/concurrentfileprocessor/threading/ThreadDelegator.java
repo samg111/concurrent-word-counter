65ba53783b4a8940455c192577ddecf396346563
samg111 <berrystump@protonmail.com>
2025-07-24 16:53:23 -0400

    feat(ThreadDelegator): stores the number of files in the fileStats variable

--- src/main/java/com/concurrentfileprocessor/threading/ThreadDelegator.java
+++ src/main/java/com/concurrentfileprocessor/threading/ThreadDelegator.java
@@ -1,13 +1,15 @@
 package com.concurrentfileprocessor.threading;
 
-import static com.concurrentfileprocessor.ConcurrentFileProcessor.inputFiles;
 import static com.concurrentfileprocessor.ConcurrentFileProcessor.fileStats;
+import static com.concurrentfileprocessor.ConcurrentFileProcessor.inputFiles;
 import com.concurrentfileprocessor.tasks.FileStatCounter;
 
 public class ThreadDelegator {
     public static void delegateTasks(){
-        Thread[] threads = new Thread[inputFiles.size()];
-        for (int i = 0; i < inputFiles.size(); i++) {
+        int numberOfFiles = inputFiles.size();
+        fileStats.numberOfFiles = numberOfFiles;
+        Thread[] threads = new Thread[numberOfFiles];
+        for (int i = 0; i < numberOfFiles; i++) {
             threads[i] = new Thread(new FileStatCounter(inputFiles.get(i), fileStats));
             threads[i].start();
         }
