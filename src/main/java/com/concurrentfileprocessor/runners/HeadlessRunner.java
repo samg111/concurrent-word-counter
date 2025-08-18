df63592b34ed56ad52b8a6e34b793f1460a6bb82
samg111 <berrystump@protonmail.com>
2025-08-18 16:32:36 -0400

    refactor(HeadlessRunner): abstract components out of the run function for increased readability

--- src/main/java/com/concurrentfileprocessor/runners/HeadlessRunner.java
+++ src/main/java/com/concurrentfileprocessor/runners/HeadlessRunner.java
@@ -1,7 +1,9 @@
 package com.concurrentfileprocessor.runners;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static com.concurrentfileprocessor.ConcurrentFileProcessor.fileStats;
@@ -15,34 +17,39 @@ public class HeadlessRunner {
         System.out.println("Running in headless mode");
 
         String currentDir = System.getProperty("user.dir");
-        File inputDirectory = new File(currentDir);
         outputFilePath = currentDir;
+        outputFilename = getOutputFilename();
 
-        outputFilename = getOutputFileName();
-        
+        File inputDirectory = new File(currentDir);
         if (inputDirectory.exists() && inputDirectory.isDirectory()) {
-            File[] files = inputDirectory.listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
-            if (files != null) {
-                inputFiles.addAll(Arrays.asList(files));
-                System.out.println("Loaded " + inputFiles.size() + " input files");
+            inputFiles = getInputFiles(inputDirectory);
+            if (inputFiles.isEmpty()) {
+                System.out.println("No text files found in current directory, ending processing");
+            } else{
+                processAndDisplayResults();
             }
         }
-        
-        if (inputFiles.isEmpty()) {
-            System.err.println("No input files found in input_files directory");
-            return;
-        }
-        
+    }
+
+    private static void processAndDisplayResults() {
         FileProcessor.processFiles();
         System.out.println("Total file count: " + fileStats.numberOfFiles);
         System.out.println("Total character count: " + fileStats.characterCount.get());
         System.out.println("Total line count: " + fileStats.lineCount.get());
         System.out.println("Total word count: " + fileStats.wordCount.size());
+    }
 
-        
+    private static List<File> getInputFiles(File directory){
+        ArrayList<File> filesArray = new ArrayList<>();
+        File[] files = directory.listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
+            if (files != null) {
+                filesArray.addAll(Arrays.asList(files));
+                System.out.println("Loaded " + filesArray.size() + " input files");
+            }
+        return filesArray;
     }
     
-    private static String getOutputFileName() {
+    private static String getOutputFilename() {
         String fileName;
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter output file name (default: processed_file_stats.txt): ");
