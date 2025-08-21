28af37582d1ae2129195000ec8e32862677fd333
samg111 <berrystump@protonmail.com>
2025-08-21 14:19:34 -0400

    refactor(FileMetricsCollector): simplify countFileComponents() by consolidating parameters into FileStats

--- src/main/java/com/concurrentfileprocessor/tasks/FileMetricsCollector.java
+++ src/main/java/com/concurrentfileprocessor/tasks/FileMetricsCollector.java
@@ -2,34 +2,28 @@ package com.concurrentfileprocessor.tasks;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import com.concurrentfileprocessor.FileStats;
 
 public class FileMetricsCollector implements Runnable {
     private final File file;
-    private final ConcurrentHashMap<String, Integer> wordCount;
-    private final AtomicInteger characterCount;
-    private final AtomicInteger lineCount;
+    private final FileStats fileStats;
 
     public FileMetricsCollector(File file, FileStats fileStats) {
         this.file = file;
-        this.wordCount = fileStats.wordCount;
-        this.characterCount = fileStats.characterCount;
-        this.lineCount = fileStats.lineCount;
+        this.fileStats = fileStats;
     }
 
     @Override
     public void run() {
         try {
-            countFileComponents(file, wordCount, characterCount, lineCount);
+            countFileComponents(file, fileStats);
         } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    public static void countFileComponents(File file, ConcurrentHashMap<String, Integer> wordCount, AtomicInteger characterCount, AtomicInteger lineCount) throws FileNotFoundException {
+    public static void countFileComponents(File file, FileStats fileStats) throws FileNotFoundException {
         try (Scanner scanner = new Scanner(file)) {
             int fileLineCount = 0;
             while (scanner.hasNextLine()) {
@@ -40,12 +34,12 @@ public class FileMetricsCollector implements Runnable {
                 for (String word : words) {
                     String cleanWord = word.toLowerCase().replaceAll("[^a-z]", "");
                     if (!cleanWord.isEmpty()) {
-                        wordCount.merge(cleanWord, 1, Integer::sum);
-                        characterCount.addAndGet(cleanWord.length());
-                    }
+                        fileStats.wordCount.merge(cleanWord, 1, Integer::sum);
+                        fileStats.characterCount.addAndGet(cleanWord.length());
+                    }  
                 }
             }
-            lineCount.addAndGet(fileLineCount);
+            fileStats.lineCount.addAndGet(fileLineCount);
         }
     }
 }
